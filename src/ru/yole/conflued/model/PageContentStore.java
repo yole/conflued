411fa7ec14298ed134fafe26c2b3effb974df460
Dmitry Jemerov <yole@jetbrains.com>
2011-07-19 05:06:35 +0200

    specify UTF-8 explicitly when loading page content

--- src/ru/yole/conflued/model/PageContentStore.java
+++ src/ru/yole/conflued/model/PageContentStore.java
@@ -3,6 +3,7 @@ package ru.yole.conflued.model;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.CharsetToolkit;
 
 import java.io.File;
 import java.io.IOException;
@@ -38,11 +39,11 @@ public class PageContentStore {
     }
 
     public String loadContent(String pageId, int version) throws IOException {
-        return FileUtil.loadFile(pagePath(pageId, version));
+        return FileUtil.loadFile(pagePath(pageId, version), CharsetToolkit.UTF8);
     }
 
     public String loadLocallyModifiedContent(String pageId) throws IOException {
-        return FileUtil.loadFile(locallyModifiedPagePath(pageId));
+        return FileUtil.loadFile(locallyModifiedPagePath(pageId), CharsetToolkit.UTF8);
     }
 
     public boolean hasContent(String pageId, int version) {
